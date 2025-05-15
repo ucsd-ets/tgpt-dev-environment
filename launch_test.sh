fbffd24e3137ca2922f6ad50e743e1e6c9433a57
bfbarry <barry.brian.f@gmail.com>
2025-05-15 15:12:15 -0700

    stuff

---
+++ launch_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+launch.sh -i ghcr.io/ucsd-ets/b2barrytest:v1 -J -s -P Always -F /home/b2barry/tgpt-dev-environment:its-dsmlp-fs03.ucsd.edu:/export/home/71/871/b2barry/tgpt-dev-environment -b
